e274912be9fb25be81e3a8600c3a9f7f185d56a4
Dencho Tsanov <d.tsanov@editcreation.com>
2026-03-11 20:06:21 +0200

    email migration

---
+++ database/migrations/2026_03_11_173011_create_emails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_id')->index();
+            $table->integer('loan_id')->index();
+            $table->integer('email_template_id')->index();
+            $table->string('receiver_email');
+            $table->string('sender_email');
+            $table->string('subject')->nullable();
+            $table->text('body');
+            $table->json('file_ids');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('sent_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emails');
+    }
+};
