fb8888c5d951862ea7c39b00aae63c6e1c3ecc92
Dencho Tsanov <d.tsanov@editcreation.com>
2026-03-11 20:24:53 +0200

    add new columns and create new table

---
+++ database/migrations/2026_03_11_180341_add_s3_columns_to_email_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('email', function (Blueprint $table) {
+            $table->string('body_s3_path')->nullable();
+            $table->string('file_s3_paths')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('email', function (Blueprint $table) {
+            $table->dropColumn('body_s3_path');
+            $table->dropColumn('file_s3_paths');
+        });
+    }
+};
