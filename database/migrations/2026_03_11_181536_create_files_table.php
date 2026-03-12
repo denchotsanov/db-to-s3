bebf08b8889165b7aab6ce2bbda77b51527e6410
Dencho Tsanov <d.tsanov@editcreation.com>
2026-03-12 19:30:43 +0200

    modify migration

--- database/migrations/2026_03_11_181536_create_files_table.php
+++ database/migrations/2026_03_11_181536_create_files_table.php
@@ -1,31 +1,19 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('files', function (Blueprint $table) {
+return new class extends Migration {
+    public function up(): void {
+        Schema::create("files", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('path'); //localpath
-            $table->double('size');
-            $table->string('type');
+            $table->string("name");
+            $table->string("path");
+            $table->double("size");
+            $table->string("type");
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('files');
+    public function down(): void {
+        Schema::dropIfExists("files");
     }
 };
